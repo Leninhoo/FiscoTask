294cf4ff05bc92339df3dd00f4520584b87b1610
Leninhoo <andre_cruz13@live.com>
2026-01-14 16:49:46 -0300

    Inclusão do objeto que representa a tabela Usuarios

---
+++ FiskTaskWF/DataBase/DbUsuarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FiscoTask.DataBase
+{
+    internal class DbUsuarios : BancoDados<DbUsuarios>
+    {
+
+        protected override string NomeTabela => "Usuarios";
+        protected override string CampoCodigo => "Id";
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Senha { get; set; }
+        public int NivelAcesso { get; set; }
+
+
+
+
+    }
+}
